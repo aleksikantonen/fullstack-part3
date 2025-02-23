060a9a50a516d8fdd39dd436391ec7ed646afcb3
Aleksi Kantonen <150439194+aleksikantonen@users.noreply.github.com>
2025-02-23 12:23:14 +0200

    osa3a: morgan token usage, POST logging

--- index.js
+++ index.js
@@ -24,9 +24,11 @@ let persons = [
         number: "39-23-6423122"
     }
 ]
-
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const info = `<div>Phonebook has info for ${persons.length} people</div>`
 const fullDate = `<p>${new Date()}</p>`
